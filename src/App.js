1823212336322c9b8455a5855c791ca07a6c9de3
LarisseLima <larisse.lima2@gmail.com>
2020-07-27 21:44:00 -0300

    Update Data

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Em 2015, quando lideranças mundiais adotaram as Metas de Desenvolvimento Sustentável ​​da Organização das Nações Unidas (as Metas Globais), elas assumiram o compromisso de empoderar todas as meninas. Apesar dos avanços registrados, precisamos continuar fazendo pressão."}
       />
 
       <Carousel
@@ -27,19 +27,19 @@ function App() {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[3]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[4]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[5]}
-      />      
+      />
 
       <Footer />
     </div>
