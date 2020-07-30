3d952ec91b8aeaace6b6a4febbf33ea2f6c03afa
LarisseLima <larisse.lima2@gmail.com>
2020-07-29 21:27:02 -0300

    Edita exports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import CadastroVideo from './pages/Cadastro/Video';
-import CadastroCategoria from './pages/Cadastro/Categoria';
-
-const Pagina404 = () => (<div>Página 404</div>)
+import CadastroVideo from './pages/Cadastro/Video'
+import CadastroCategoria from './pages/Cadastro/Categoria'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} exact></Route>
-      <Route path="/cadastro/video" component={CadastroVideo}></Route>
-      <Route path="/cadastro/categoria" component={CadastroCategoria}></Route>
-      
-      <Route component={Pagina404}></Route>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={() => (<div>Página 404</div>)} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
